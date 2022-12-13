d53942f7c182918cc639cf3c1e12433e22309c0f
Sergey Gulaga <ry6ens@gmail.com>
2022-12-13 20:07:40 +0200

    add header

---
+++ src/components/icons/Close/Close.jsx
@@ -0,0 +1,7 @@
+export default function CloseIcon() {
+  return (
+    <svg viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="m1 1 12 12M1 13 13 1" stroke="#52555F" stroke-width="2" />
+    </svg>
+  );
+}
