bf9d97eeda008b0105ececda570867964428dfd1
Sergey Gulaga <ry6ens@gmail.com>
2022-12-29 10:40:47 +0200

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,7 +16,7 @@ import rootReducer from './rootReducer';
 const persistConfig = {
   key: 'auth',
   storage,
-  whitelist: ['auth'],
+  // whitelist: ['auth'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
